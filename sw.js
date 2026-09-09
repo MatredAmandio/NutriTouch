bc9ef3c31a0f1d3649f7fc0bfbb0bd6cd7a00936
MatredAmandio <Matredamandio@hotmail.com>
2026-09-09 13:45:05 -0300

    Atualiza cache para versão 10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nutritouch-v9';
+const CACHE='nutritouch-v10';
 const CORE=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('nutritouch-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
