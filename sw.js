7a3da001ca4c2db8b594c126b334adbf3d00da8f
MatredAmandio <Matredamandio@hotmail.com>
2026-09-11 19:59:42 -0300

    pwa: cache food expansion and quantified modules

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutritouch-v16-5';
+const CACHE = 'nutritouch-v16-6';
 const CORE = [
   './',
   './index.html',
@@ -22,6 +22,8 @@ const CORE = [
   './js/meals/generator.js',
   './js/meals/nutrition.js',
   './js/meals/quantified-recipes.js',
+  './js/meals/quantified-recipes-3.3.js',
+  './js/meals/quantified-library.js',
   './js/meals/quantified-generator.js',
   './js/data/foods.js',
   './js/ui/dom.js',
@@ -31,7 +33,8 @@ const CORE = [
   './js/ui/foods.js',
   './js/ui/evolution.js',
   './data/foods.json',
-  './data/foods-taco.json'
+  './data/foods-taco.json',
+  './data/foods-3.3.json'
 ];
 
 self.addEventListener('install', event => {
@@ -69,7 +72,7 @@ self.addEventListener('fetch', event => {
     return;
   }
 
-  if (url.pathname.endsWith('/data/foods.json') || url.pathname.endsWith('/data/foods-taco.json')) {
+  if (url.pathname.endsWith('/data/foods.json') || url.pathname.endsWith('/data/foods-taco.json') || url.pathname.endsWith('/data/foods-3.3.json')) {
     event.respondWith(
       fetch(event.request)
         .then(response => cacheSuccessful(event.request, response))
