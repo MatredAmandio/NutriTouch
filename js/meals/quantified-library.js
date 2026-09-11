e240073d7b8e54cce5883bd41c737b0fa80d42a9
MatredAmandio <Matredamandio@hotmail.com>
2026-09-11 19:58:55 -0300

    meals: aggregate quantified recipe modules

---
+++ js/meals/quantified-library.js
@@ -0,0 +1,7 @@
+import { QUANTIFIED_RECIPES as CORE_RECIPES } from './quantified-recipes.js';
+import { QUANTIFIED_RECIPES_3_3 } from './quantified-recipes-3.3.js';
+
+export const QUANTIFIED_RECIPES = Object.freeze([
+  ...CORE_RECIPES,
+  ...QUANTIFIED_RECIPES_3_3
+]);
