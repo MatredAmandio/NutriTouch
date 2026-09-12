3c1a2f4d6195b6a790e585d1b726472328134f4b
MatredAmandio <Matredamandio@hotmail.com>
2026-09-12 10:05:25 -0300

    meals: include coffee tea breakfast recipes

--- js/meals/quantified-library.js
+++ js/meals/quantified-library.js
@@ -1,9 +1,11 @@
 import { QUANTIFIED_RECIPES as CORE_RECIPES } from './quantified-recipes.js';
 import { QUANTIFIED_RECIPES_3_3 } from './quantified-recipes-3.3.js';
 import { QUANTIFIED_CHEESE_RECIPES } from './quantified-recipes-cheese.js';
+import { QUANTIFIED_BEVERAGE_RECIPES } from './quantified-recipes-beverages.js';
 
 export const QUANTIFIED_RECIPES = Object.freeze([
   ...CORE_RECIPES,
   ...QUANTIFIED_RECIPES_3_3,
-  ...QUANTIFIED_CHEESE_RECIPES
+  ...QUANTIFIED_CHEESE_RECIPES,
+  ...QUANTIFIED_BEVERAGE_RECIPES
 ]);
