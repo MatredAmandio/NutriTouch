574ad5f1e56db9c1f83ef525215066d0977ec6eb
MatredAmandio <Matredamandio@hotmail.com>
2026-09-11 23:43:10 -0300

    meals: include validated cheese recipes

--- js/meals/quantified-library.js
+++ js/meals/quantified-library.js
@@ -1,7 +1,9 @@
 import { QUANTIFIED_RECIPES as CORE_RECIPES } from './quantified-recipes.js';
 import { QUANTIFIED_RECIPES_3_3 } from './quantified-recipes-3.3.js';
+import { QUANTIFIED_CHEESE_RECIPES } from './quantified-recipes-cheese.js';
 
 export const QUANTIFIED_RECIPES = Object.freeze([
   ...CORE_RECIPES,
-  ...QUANTIFIED_RECIPES_3_3
+  ...QUANTIFIED_RECIPES_3_3,
+  ...QUANTIFIED_CHEESE_RECIPES
 ]);
